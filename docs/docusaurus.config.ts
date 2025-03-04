46baaf5a8f791aa92dde82e886fbe7542e769fce
Laurie C <133490867+lmcrean@users.noreply.github.com>
2025-03-04 20:43:35 +0000

    Modify Docusaurus config to handle broken links more gracefully

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: 'lmcrean', // Usually your GitHub org/user name.
   projectName: 'tributary', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
